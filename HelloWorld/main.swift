5bf9b6dfad35d61ab7424332c61b39a7c3ae2e98
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2023-11-09 12:05:24 -0500

    Going through the tutorials tooperator section

--- HelloWorld/main.swift
+++ HelloWorld/main.swift
@@ -4,8 +4,60 @@
 //
 //  Created by Matt Crowe on 11/8/23.
 //
+//test
+
+/* Testing comments*/
 
 import Foundation
 
-print("Hello, World!")
+var age = 21
+var name = "Birdman"
+var money:Float
+
+money = 20.4
+let bankName = "Chase Bank"
+let bankNumber = 23.0
+
+age += 2
+age -= 1
+age *= 2
+
+if(age < 18)
+{
+    print("Get out of the bar")
+}else{
+    print("You are allowed in")
+}
+
+//range operators which is a unique feature for swift
+
+let names = ["Birdman","Michael","Larry"]
+let x:ClosedRange = 0...1
+let y = 8
+
+let kidAge = 12
+let maximumAgeLimit = 13
+
+if(...maximumAgeLimit).contains(kidAge){
+    print("The kid is allowed into the party")
+}
+
+
+
+print("Lets print a range which is a unique operator for swift")
+print(names[x])
+
+print("The bank name is: " + bankName + " and the bank number is: \(bankNumber)"   )
+
+print("Hello, World! \nTesting the new line character here")
+
+print("My name is: " + name + "  and my age is: \(age)")
+
+
+
+
+print("The amount of money I have as a programmer in my bank account currently: \(money)")
+
+
+
 
--- README.md
+++ README.md
@@ -4,3 +4,13 @@ This is just to follow along with a tutorial from geeks for geeks
 Learning my first Apple Hello World Program
 
 https://www.geeksforgeeks.org/swift-hello-world-program/
+
+The above Hello World article was so short I found another source W3schools 
+and I continued to code to learn the syntax, environment etc. 
+
+https://www.w3schools.in/swift/overview
+
+To learn ranges unique to swift
+https://www.avanderlee.com/swift/ranges-explained/
+
+
